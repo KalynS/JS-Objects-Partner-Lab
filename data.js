859bf67be930dee280a51fabb6ac0e5990431e11
Kalyn Smith <kalyn@Kalyns-MacBook-Air.local>
2020-12-07 15:59:06 -0500

    First part

--- data.js
+++ data.js
@@ -4,3 +4,10 @@ let userData = [
     ["Froilanda", "Miran", "Student", "Java"],
     ["Val", "Fragier", "Instructor", "JavaScript"],
 ];
+
+let myButton = document.getElementById("button")
+myButton.addEventListener("click",clickMe)
+function clickMe(){
+ console.log(test)
+}
+
